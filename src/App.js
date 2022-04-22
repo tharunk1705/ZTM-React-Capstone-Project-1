5a673f74c97a269af93988b6947f932c8ee5cb9d
Tharun Kumar <tharunk1705@gmail.com>
2022-04-23 00:11:18 +0530

    Firebase authentication setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/routes/home/home.component";
 import Navigation from "./components/routes/Navigation/navigation.component";
+import SignIn from "./components/routes/sign-in/sign-in.component";
 
 const Shop = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
+        <Route path="signin" element={<SignIn />} />
       </Route>
     </Routes>
   );
---
+++ src/components/routes/sign-in/sign-in.component.jsx
@@ -0,0 +1,20 @@
+import {
+  signInWithGooglePopup,
+  createUserDocumentFromAuth,
+} from "../../../utils/firebase/firebase.utils";
+
+const SignIn = () => {
+  const logGoogleUser = async () => {
+    const { user } = await signInWithGooglePopup();
+    const userDocRef = await createUserDocumentFromAuth(user);
+    console.log(userDocRef);
+  };
+  return (
+    <div>
+      SignIn Page
+      <button onClick={logGoogleUser}>Sign in with Google</button>
+    </div>
+  );
+};
+
+export default SignIn;

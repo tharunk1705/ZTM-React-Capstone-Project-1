3d3f4e2a7cf37e59651c11fce5ecb752eefcdb9b
Tharun Kumar <tharunk1705@gmail.com>
2022-04-24 17:03:02 +0530

    Implemented cart functionalities

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,11 +1,18 @@
 import "./cart-dropdown.styles.scss";
 import Button from "./../button/button.component";
-import React from "react";
+import CartItem from "../cart-item/cart-item.component";
+import { CartContext } from "../../context/cart.context";
+import { useContext } from "react";
 
 const CartDropdown = () => {
+  const { cartItems } = useContext(CartContext);
   return (
     <div className="cart-dropdown-container">
-      <div className="cart-items"></div>
+      <div className="cart-items">
+        {cartItems.map((item) => (
+          <CartItem cartItem={item} />
+        ))}
+      </div>
       <Button>Checkout</Button>
     </div>
   );
